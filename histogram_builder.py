11d76d0679bf36ea6ce4f916dd3d159b54bed045
leolincoln <leoliu@u.northwestern.edu>
2016-06-01 11:48:14 -0500

    minuses19.png

--- histogram_builder.py
+++ histogram_builder.py
@@ -1,14 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-#load data
-data = np.loadtxt('heat3_19.csv')
-#flatten data
-data = np.ravel(data)
-#filter data
-data = data[data<2]
-pd_data = pd.DataFrame(data)
-plt.figure()
-pd_data.plot.hist(bins=20)
-plt.savefig('heat3_test2.png')
+import sys
+
+if __name__=='__main__':
+    if len(sys.argv)<1:
+        print 'error: python histbuilder name'
+        file_name = 'heat3_19.csv'
+    else:
+        file_name = sys.argv[1]
+    print 'using file_name',file_name
+    #load data
+    data = np.loadtxt(file_name)
+    for i in range(len(data)):
+        data[i][i]=0
+    #flatten data
+    data = np.ravel(data)
+    #filter data
+    #data = data[data<2]
+    pd_data = pd.DataFrame(data)
+    plt.figure()
+    pd_data.plot.hist(bins=20)
+    plt.savefig(file_name[:-4]+'.png')
